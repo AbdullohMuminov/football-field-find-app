ed1849489cf0cd318fafdc44321cbad1ee48537d
muminovabdulloh <muminovabdullokh@gmail.com>
2024-08-18 22:30:58 +0500

    Abdulloh Muminov, 2-commit, Folder structure has created.

---
+++ lib/src/core/widgets/app_material_context.dart
@@ -0,0 +1,22 @@
+import "package:flutter/material.dart";
+import "../../feature/main/main_screen.dart";
+import "../../feature/settings/inherited_theme_notifier.dart";
+import "../../feature/settings/theme_controller.dart";
+
+final ThemeController themeController = ThemeController();
+
+class AppMaterialContext extends StatelessWidget {
+  const AppMaterialContext({super.key});
+
+  @override
+  Widget build(BuildContext context) => InheritedThemeNotifier(
+        themeController: themeController,
+        child: Builder(
+          builder: (context) => MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: InheritedThemeNotifier.maybeOf(context)?.theme,
+            home: const MainScreen(),
+          ),
+        ),
+      );
+}
