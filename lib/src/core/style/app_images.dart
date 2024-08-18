2b396274e7b1cd834176d0c2a8fb82ae9c8be20e
muminovabdulloh <muminovabdullokh@gmail.com>
2024-08-19 00:10:48 +0500

    Abdulloh Muminov, 5-commit, image styles have created.

--- lib/src/core/style/app_images.dart
+++ lib/src/core/style/app_images.dart
@@ -7,24 +7,64 @@ final class AppImages {
 
   // *** svg images
 
-  static final monitoring = SvgPicture.asset(
-    "",
-    height: 24.w,
-    width: 24.w,
+  static final mapIconSelected = SvgPicture.asset(
+    "assets/icons/map_icon_selected.svg",
+    height: 16.h,
+    width: 16.w,
   );
 
-  // *** png images
-
-  static final sitting = Image.asset(
-    "",
-    height: 216.w,
-    width: 216.w,
+  static final mapIconUnselected = SvgPicture.asset(
+    "assets/icons/map_icon_unselected.svg",
+    height: 16.h,
+    width: 16.w,
   );
 
-  static final defaultProfileImage = Image.asset(
-    "",
-    // width: 31.w,
-    // height: 31.h,
-    fit: BoxFit.cover,
+  static final listIconSelected = SvgPicture.asset(
+    "assets/icons/list_icon_selected.svg",
+    height: 14.65.h,
+    width: 16.w,
   );
+
+  static final listIconUnselected = SvgPicture.asset(
+    "assets/icons/list_icon_unselected.svg",
+    height: 14.65.h,
+    width: 16.w,
+  );
+
+  static final exploreIconSelected = SvgPicture.asset(
+    "assets/icons/explore_icon_selected.svg",
+    height: 20.h,
+    width: 25.w,
+  );
+
+  static final favoriteIcon = SvgPicture.asset(
+    "assets/icons/favorite_icon_unselected.svg",
+    height: 20.h,
+    width: 23.w,
+  );
+
+  static final accountIconUnselected = SvgPicture.asset(
+    "assets/icons/account_icon_unselected.svg",
+    height: 20.h,
+    width: 16.w,
+  );
+
+  static final locationIcon = SvgPicture.asset(
+    "assets/icons/location_icon.svg",
+    height: 22.h,
+    width: 22.w,
+  );
+
+  static final directionIcon = SvgPicture.asset(
+    "assets/icons/direction_icon.svg",
+    height: 18.h,
+    width: 18.w,
+  );
+
+  static final stadiumLocationIcon = SvgPicture.asset(
+    "assets/icons/stadium_location_icon.svg",
+    height: 48.h,
+    width: 42.w,
+  );
+
 }
